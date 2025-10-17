8e57bb1bdb6ca38254e5d65a186c58663d772138
Aleksey Komarov <q4arus@ya.ru>
2025-10-17 14:55:30 +0300

    don't forget to call SDL_InitSubSystem on Linux

--- source.cpp
+++ source.cpp
@@ -2,9 +2,16 @@
 #include <dxgi.h>
 #ifdef _WIN32
 #pragma comment (lib, "DXGI.lib")
+#else
+#include <SDL2/SDL.h>
 #endif
 
 int main() {
+
+#ifndef _WIN32
+	SDL_InitSubSystem(SDL_INIT_EVENTS | SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
+#endif
+
 	IDXGIFactory* pFactory1 = nullptr;
 	HRESULT hr1 = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory1));
 	if (SUCCEEDED(hr1))

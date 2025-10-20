b0fa503974fc3783fc634f476f5c59561d19d3ee
Aleksey Komarov <q4arus@ya.ru>
2025-10-20 13:00:42 +0300

    add D3DCreateBlob call (for vkd3d test)

--- source.cpp
+++ source.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <d3dcompiler.h>
 #include <dxgi.h>
 #ifdef _WIN32
 #pragma comment (lib, "DXGI.lib")
+#pragma comment (lib, "d3dcompiler.lib")
 #else
 #include <SDL2/SDL.h>
 #endif
@@ -12,18 +14,23 @@ int main() {
 	SDL_InitSubSystem(SDL_INIT_EVENTS | SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
 #endif
 
-	IDXGIFactory* pFactory1 = nullptr;
-	HRESULT hr1 = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory1));
-	if (SUCCEEDED(hr1))
+	ID3DBlob* codeBufferBlob = nullptr;
+	HRESULT hRet = D3DCreateBlob(5760, &codeBufferBlob);
+	if (FAILED(hRet))
 	{
-		std::cout << "Success on 1st CreateDXGIFactory" << std::endl;
+		std::cout << "Failed to D3DCreateBlob(5760, &codeBufferBlob)" << std::endl;
+		return 0;
+	}
+	else
+	{
+		std::cout << "Size of blob from D3DCreateBlob(5760, &codeBufferBlob)==" << codeBufferBlob->GetBufferSize() << std::endl;
 	}
 
-	IDXGIFactory* pFactory2 = nullptr;
-	HRESULT hr2 = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory1));
-	if (SUCCEEDED(hr2))
+	IDXGIFactory* pFactory = nullptr;
+	HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&pFactory));
+	if (SUCCEEDED(hr))
 	{
-		std::cout << "Success on 2nd CreateDXGIFactory" << std::endl;
+		std::cout << "Success on CreateDXGIFactory" << std::endl;
 	}
 
 	return 0;

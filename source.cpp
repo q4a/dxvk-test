16ec3647cb403ecd78c1d79e8d7e0a9bfdaab1f3
Aleksey Komarov <q4arus@ya.ru>
2025-10-26 14:39:18 +0300

    fix Windows build, add D3DStripShader call in Windows

--- source.cpp
+++ source.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
+#ifdef _WIN32
+#include <d3dcompiler.h>
+#include <dxgi.h>
+#pragma comment (lib, "d3dcompiler.lib")
+#else // _WIN32
 #include <vkd3d_utils.h>
 #include <vkd3d_d3dcompiler.h>
+#endif // _WIN32
 
 int main() {
 	ID3DBlob* codeBufferBlob = nullptr;
 	HRESULT hRet = D3DCreateBlob(5760, &codeBufferBlob);
-	if (FAILED(hRet))
-	{
+	if (FAILED(hRet)) {
 		std::cout << "Failed to D3DCreateBlob(5760, &codeBufferBlob)" << std::endl;
 		return 0;
-	}
-	else
-	{
+	} else {
 		std::cout << "Size of blob from D3DCreateBlob(5760, &codeBufferBlob)==" << codeBufferBlob->GetBufferSize() << std::endl;
 	}
 
 	ID3DBlob* stripBuffer = nullptr;
-	SIZE_T stripFlags = D3DCOMPILER_STRIP_REFLECTION_DATA | D3DCOMPILER_STRIP_PRIVATE_DATA;
-	//D3DStripShader(codeBufferBlob->GetBufferPointer(), codeBufferBlob->GetBufferSize(), stripFlags, &stripBuffer);
+#ifdef _WIN32
+	hRet = D3DStripShader(codeBufferBlob->GetBufferPointer(), codeBufferBlob->GetBufferSize(), D3DCOMPILER_STRIP_PRIVATE_DATA, &stripBuffer);
+	if (FAILED(hRet)) {
+		std::cout << "As expected: failed to call D3DStripShader" << std::endl;
+	}
+#endif
 
 	return 0;
 }
